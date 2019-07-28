6b89043c59d904b15b540b510da4202595fa922d
Felipe Souza <felipessouza2019@gmail.com>
2019-07-28 19:49:54 -0300

    slick slider

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,40 @@ $(document).ready(function() {
 
     userFeed.run();
 
+    $(".galeria-slider").slick({
+      infinite: true,
+      autoplay: true,
+      speed: 200,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      ]
+
+    });
+
     //
     // // This will create a single gallery from all elements that have class "gallery-item"
     // $('.gallery').magnificPopup({
