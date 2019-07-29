7d0860a1c85084ca16effa39c6826ddbcb26f174
Felipe Souza <felipessouza2019@gmail.com>
2019-07-28 23:38:20 -0300

    font

--- js/custom.js
+++ js/custom.js
@@ -3,52 +3,54 @@ $(document).ready(function() {
 
     var userFeed = new Instafeed({
         get: 'user',
-        userId: '655593012',
+        // userId: '655593012',
+        userId:'13441161167',
         limit: 4,
         resolution: 'standard_resolution',
-        accessToken: '655593012.1677ed0.af392e0a3ca24e6dac2356327f8063d5',
+        // accessToken: '655593012.1677ed0.af392e0a3ca24e6dac2356327f8063d5',
+        accessToken: '13441161167.1677ed0.4503057340ee42d9b69a10e0725773d9',
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
 
 
     userFeed.run();
-
-    $(".galeria-slider").slick({
-      infinite: true,
-      autoplay: true,
-      speed: 200,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      nextArrow: '<i class="fa fas fa-chevron-circle-right"></i>',
-      prevArrow: '<i class="fa fas fa-chevron-circle-left"></i>',
-      responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      ]
-
-    });
+    //
+    // $(".8galeria-slider").slick({
+    //   infinite: true,
+    //   autoplay: true,
+    //   speed: 2000,
+    //   slidesToShow: 4,
+    //   slidesToScroll: 4,
+    //   nextArrow: '<i class="fa fas fa-chevron-circle-right"></i>',
+    //   prevArrow: '<i class="fa fas fa-chevron-circle-left"></i>',
+    //   responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 1,
+    //       infinite: true,
+    //
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 1
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+    //   ]
+    //
+    // });
 
     //
     // // This will create a single gallery from all elements that have class "gallery-item"
